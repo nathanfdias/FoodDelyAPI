79bad3c220368042b6e792abff33085acc67e235
Nathan Dias <nathanfaria9@gmail.com>
2023-02-10 12:15:59 -0300

    Model Purchase - UserPurchase

---
+++ src/main/java/com/study/deliveryFoodapi/Enums/EStatus.java
@@ -0,0 +1,8 @@
+package com.study.deliveryFoodapi.Enums;
+
+public enum EStatus {
+    EM_ANDAMENTO,
+	A_CAMINHO,
+	ENTREGUE,
+	CANCELADO;
+}
---
+++ src/main/java/com/study/deliveryFoodapi/model/Purchase.java
@@ -0,0 +1,31 @@
+package com.study.deliveryFoodapi.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "purchase")
+public class Purchase {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private int quantity;
+
+    private double unit_price;
+
+    @ManyToOne
+    @JoinColumn(name = "product_id")
+    private Product product;
+
+}
